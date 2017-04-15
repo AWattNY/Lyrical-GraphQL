6d9f06d2891ba9fd5b8ea49b1be73f932432e532
Adam Watt <awattny@me.com>
2017-04-14 19:58:46 -0700

    set up express server and add mongoDB credentials

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://adamw:adamw@ds031608.mlab.com:31608/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
